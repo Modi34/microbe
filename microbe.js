8ccfe6c17ea5ad599eceab614a8c1982d08f018a
Oleg Buylov <7198509+Modi34@users.noreply.github.com>
2020-05-02 22:21:10 +0300

    Update microbe.js

--- microbe.js
+++ microbe.js
@@ -3,8 +3,8 @@ const microbe = {
   new(tag, params) {
     let node = document.createElement(tag);
     for (let param of params) {
-      if (param.nodeName || param.length) {
-        node.appendChild( param.nodeName? param : document.createTextNode(param) )
+      if (param.valueOf) {
+        node.appendChild( param.after ? param : document.createTextNode(param) )
       } else {
         for (let attr in param) {
           node.setAttribute(attr, param[attr])
