5b179de512193e0f1e33da9769a05c1ea35f3dd5
Oleg Buylov <7198509+Modi34@users.noreply.github.com>
2020-05-04 17:33:11 +0300

    replaced loop with Object.assign, removed recursion

--- microbe.js
+++ microbe.js
@@ -1,24 +1,11 @@
-const microbe = {
-  init: (...tags) => tags.map(tag => window[tag] = (...params) => microbe.new(tag, params)),
-  new(tag, params) {
+const microbe = (...tags) => {
+  tags.map(tag => window[tag] = (...params) => {
     let node = document.createElement(tag);
     params.flat().map( param => {
-      if (param.constructor==Object) {
-        microbe.parse(param, node)
-      } else {
+        param.constructor==Object? Object.assign(node, param) :
         param.call? param(node) :
         node.appendChild( param.after ? param : document.createTextNode(param) )
-      }
     })
     return node
-  },
-  parse(obj, node){
-    for(let i in obj){
-     if(obj[i].constructor == Object){
-      microbe.parse(obj[i], node[i])
-     }else{
-      node[i] = obj[i]
-     }
-    }
-   }
+  }
 }
