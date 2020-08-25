5f989c42fc4d0f7c6db1a35e1736ac4adf9c3a20
Oleg Buylov <7198509+Modi34@users.noreply.github.com>
2020-08-26 00:03:28 +0300

    Update microbe.js

--- microbe.js
+++ microbe.js
@@ -2,7 +2,7 @@ const microbe = (...tags) => {
   tags.map(tag => this[tag] = (...params) => {
     let node = document.createElement(tag);
     params.map( param => {
-        param.constructor==Object? Object.assign(node, param) :
+        param+'' == {} ? Object.assign(node, param) :
         param.call? param(node) :
         node.append( param )
     })
